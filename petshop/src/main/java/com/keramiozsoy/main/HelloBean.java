137e4df62365708a99c73907e7a58b2ed85bf2da
Kerami Ozsoy <keramiozsoy@hotmail.com>
2017-11-17 17:53:17 +0200

    created bean

---
+++ petshop/src/main/java/com/keramiozsoy/main/HelloBean.java
@@ -0,0 +1,37 @@
+package com.keramiozsoy.main;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private String input;
+  private String output;
+
+  public void submit() {
+    output = "You submitted: " + input;
+  }
+
+  public String getInput() {
+    return input;
+  }
+
+  public void setInput(String input) {
+    this.input = input;
+  }
+
+  public String getOutput() {
+    return output;
+  }
+
+  public void setOutput(String output) {
+    this.output = output;
+  }
+
+}
