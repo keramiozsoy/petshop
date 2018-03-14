2c15d9bd427ba6a143ffa2662cebf94bd4c4c5c1
Kerami Ozsoy <keramiozsoy@hotmail.com>
2018-03-14 20:11:12 +0200

    JDBCUtil class has a unit test file

---
+++ petshop/src/test/java/com/petshop/core/util/JDBCUtilTest.java
@@ -0,0 +1,44 @@
+package com.petshop.core.util;
+
+import com.petshop.core.enums.Enums;
+import java.sql.Connection;
+import junit.framework.TestCase;
+
+/**
+ * @since 14.03.2018
+ * @author ozsoy.kerami
+ * 
+ */
+public class JDBCUtilTest extends TestCase {
+    
+    public JDBCUtilTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of OpenJdbcConnection method, of class JDBCUtil.
+     */
+    public void testOpenJdbcConnection() throws Exception {
+        System.out.println(this.getClass().getName() +" --> " +"OpenJdbcConnection");
+        Object driver = Enums.DatabaseDriver.APACHE_DERBY.toValue();
+        String dbPath = Enums.DatabaseInfo.DB_PATH_APACHE_DERBY.toValue().toString();
+        String user = Enums.DatabaseInfo.USER.toValue().toString();
+        String password = Enums.DatabaseInfo.PASSWORD.toValue().toString();
+        
+        Connection result = JDBCUtil.OpenJdbcConnection(driver, dbPath, user, password);
+        
+        assertNotNull(result);
+
+    }
+    
+}
